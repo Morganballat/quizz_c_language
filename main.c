236ac10ace3e2e1a1d1b630f3ceaeca3748f1fc2
Morganballat <mgp.ballat@gmail.com>
2024-05-28 10:12:24 +0200

    add valgrind, add check if input is correct

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "quiz.h"
 
-void display_ascii() {
+void display_ascii()
+{
     printf(" ________              ___________      .__      .__        \n");
     printf(" \\______ \\   _______  _\\__    __/_______|__|__  _|__|____   \n");
     printf("  |   |  \\_/ __ \\  \\/ /  |    |  \\_  __ \\  \\  \\/ /  \\__  \\  \n");
@@ -12,7 +13,29 @@ void display_ascii() {
     printf("\n");
 }
 
-int main() {
+int getUserChoice()
+{
+    int choice;
+    while (1)
+    {
+        printf("Choix: ");
+        if (scanf("%d", &choice) != 1 || choice < 1 || choice > 5)
+        {
+            printf("Choix invalide. Veuillez entrer un nombre entre 1 et 5.\n");
+            while (getchar() != '\n')
+                ; // vider le tampon d'entrée
+        }
+        else
+        {
+            while (getchar() != '\n')
+                ; // vider le tampon d'entrée
+            return choice;
+        }
+    }
+}
+
+int main()
+{
     display_ascii();
 
     printf(" ____________________________________________________ \n");
@@ -28,33 +51,32 @@ int main() {
     printf("3. C#\n");
     printf("4. C\n");
     printf("5. Javascript\n");
-    printf("Choix: ");
-    
-    int choice;
-    scanf("%d", &choice);
+
+    int choice = getUserChoice();
 
     int size;
     QuizQuestion *questions;
 
-    switch (choice) {
-        case 1:
-            questions = createPythonQuiz(&size);
-            break;
-        case 2:
-            questions = createJavaQuiz(&size);
-            break;
-        case 3:
-            questions = createCSharpQuiz(&size);
-            break;
-        case 4:
-            questions = createCQuiz(&size);
-            break;
-        case 5:
-            questions = createJavaScriptQuiz(&size);
-            break;
-        default:
-            printf("Choix invalide.\n");
-            return 1;
+    switch (choice)
+    {
+    case 1:
+        questions = createPythonQuiz(&size);
+        break;
+    case 2:
+        questions = createJavaQuiz(&size);
+        break;
+    case 3:
+        questions = createCSharpQuiz(&size);
+        break;
+    case 4:
+        questions = createCQuiz(&size);
+        break;
+    case 5:
+        questions = createJavaScriptQuiz(&size);
+        break;
+    default:
+        printf("Choix invalide.\n");
+        return 1;
     }
 
     printf("Appuyez sur la touche entrée pour commencer...\n");
--- quiz.c
+++ quiz.c
@@ -3,167 +3,161 @@
 #include <string.h>
 #include "quiz.h"
 
-<<<<<<< Updated upstream
-QuizQuestion *createQuiz(int *size)
-{
-    *size = 10;
-=======
 // Questions pour le thème Python
-QuizQuestion* createPythonQuiz(int *size) {
+QuizQuestion *createPythonQuiz(int *size)
+{
     *size = 3;
->>>>>>> Stashed changes
     QuizQuestion *questions = malloc(*size * sizeof(QuizQuestion));
 
     questions[0].question = strdup("Quel est le résultat de l'expression suivante en Python : 5 * 3 + 2 / 2 - 1?");
-    questions[0].options[0] = strdup("A. 15");
-    questions[0].options[1] = strdup("B. 7");
-    questions[0].options[2] = strdup("C. 8");
-    questions[0].options[3] = strdup("D. 9");
-    questions[0].correct_option = 'A';
+    questions[0].options[0] = strdup("a. 15");
+    questions[0].options[1] = strdup("b. 7");
+    questions[0].options[2] = strdup("c. 8");
+    questions[0].options[3] = strdup("d. 9");
+    questions[0].correct_option = 'a';
 
     questions[1].question = strdup("Quel mot-clé est utilisé pour définir une fonction en Python?");
-    questions[1].options[0] = strdup("A. func");
-    questions[1].options[1] = strdup("B. def");
-    questions[1].options[2] = strdup("C. function");
-    questions[1].options[3] = strdup("D. define");
-    questions[1].correct_option = 'B';
+    questions[1].options[0] = strdup("a. func");
+    questions[1].options[1] = strdup("b. def");
+    questions[1].options[2] = strdup("c. function");
+    questions[1].options[3] = strdup("d. define");
+    questions[1].correct_option = 'b';
 
-    questions[2].question = strdup("Comment créer une liste en Python?");
-    questions[2].options[0] = strdup("A. []");
-    questions[2].options[1] = strdup("B. {}");
-    questions[2].options[2] = strdup("C. ()");
-    questions[2].options[3] = strdup("D. <>");
-    questions[2].correct_option = 'A';
+    questions[2].question = strdup("comment créer une liste en Python?");
+    questions[2].options[0] = strdup("a. []");
+    questions[2].options[1] = strdup("b. {}");
+    questions[2].options[2] = strdup("c. ()");
+    questions[2].options[3] = strdup("d. <>");
+    questions[2].correct_option = 'a';
 
     return questions;
 }
 
-<<<<<<< Updated upstream
-void freeQuiz(QuizQuestion *questions, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-=======
 // Questions pour le thème Java
-QuizQuestion* createJavaQuiz(int *size) {
+QuizQuestion *createJavaQuiz(int *size)
+{
     *size = 3;
     QuizQuestion *questions = malloc(*size * sizeof(QuizQuestion));
 
     questions[0].question = strdup("Quel est le type de retour de la méthode 'main' en Java?");
-    questions[0].options[0] = strdup("A. int");
-    questions[0].options[1] = strdup("B. void");
-    questions[0].options[2] = strdup("C. String");
-    questions[0].options[3] = strdup("D. char");
-    questions[0].correct_option = 'B';
+    questions[0].options[0] = strdup("a. int");
+    questions[0].options[1] = strdup("b. void");
+    questions[0].options[2] = strdup("c. String");
+    questions[0].options[3] = strdup("d. char");
+    questions[0].correct_option = 'b';
 
     questions[1].question = strdup("Quel mot-clé est utilisé pour hériter d'une classe en Java?");
-    questions[1].options[0] = strdup("A. extends");
-    questions[1].options[1] = strdup("B. implements");
-    questions[1].options[2] = strdup("C. inherits");
-    questions[1].options[3] = strdup("D. extendsClass");
-    questions[1].correct_option = 'A';
+    questions[1].options[0] = strdup("a. extends");
+    questions[1].options[1] = strdup("b. implements");
+    questions[1].options[2] = strdup("c. inherits");
+    questions[1].options[3] = strdup("d. extendsClass");
+    questions[1].correct_option = 'a';
 
     questions[2].question = strdup("Quel est le package de base de Java?");
-    questions[2].options[0] = strdup("A. java.lang");
-    questions[2].options[1] = strdup("B. java.util");
-    questions[2].options[2] = strdup("C. java.io");
-    questions[2].options[3] = strdup("D. java.net");
-    questions[2].correct_option = 'A';
+    questions[2].options[0] = strdup("a. java.lang");
+    questions[2].options[1] = strdup("b. java.util");
+    questions[2].options[2] = strdup("c. java.io");
+    questions[2].options[3] = strdup("d. java.net");
+    questions[2].correct_option = 'a';
 
     return questions;
 }
 
 // Questions pour le thème C#
-QuizQuestion* createCSharpQuiz(int *size) {
+QuizQuestion *createCSharpQuiz(int *size)
+{
     *size = 3;
     QuizQuestion *questions = malloc(*size * sizeof(QuizQuestion));
 
     questions[0].question = strdup("Quel mot-clé est utilisé pour définir une propriété en C#?");
-    questions[0].options[0] = strdup("A. property");
-    questions[0].options[1] = strdup("B. prop");
-    questions[0].options[2] = strdup("C. var");
-    questions[0].options[3] = strdup("D. get; set;");
-    questions[0].correct_option = 'D';
+    questions[0].options[0] = strdup("a. property");
+    questions[0].options[1] = strdup("b. prop");
+    questions[0].options[2] = strdup("c. var");
+    questions[0].options[3] = strdup("d. get; set;");
+    questions[0].correct_option = 'd';
 
     questions[1].question = strdup("Quelle méthode est utilisée pour commencer un thread en C#?");
-    questions[1].options[0] = strdup("A. Start()");
-    questions[1].options[1] = strdup("B. Run()");
-    questions[1].options[2] = strdup("C. Begin()");
-    questions[1].options[3] = strdup("D. Execute()");
-    questions[1].correct_option = 'A';
+    questions[1].options[0] = strdup("a. Start()");
+    questions[1].options[1] = strdup("b. Run()");
+    questions[1].options[2] = strdup("c. Begin()");
+    questions[1].options[3] = strdup("d. Execute()");
+    questions[1].correct_option = 'a';
 
-    questions[2].question = strdup("Comment déclare-t-on une constante en C#?");
-    questions[2].options[0] = strdup("A. const int x = 10;");
-    questions[2].options[1] = strdup("B. final int x = 10;");
-    questions[2].options[2] = strdup("C. static int x = 10;");
-    questions[2].options[3] = strdup("D. define x = 10;");
-    questions[2].correct_option = 'A';
+    questions[2].question = strdup("comment déclare-t-on une constante en C#?");
+    questions[2].options[0] = strdup("a. const int x = 10;");
+    questions[2].options[1] = strdup("b. final int x = 10;");
+    questions[2].options[2] = strdup("c. static int x = 10;");
+    questions[2].options[3] = strdup("d. define x = 10;");
+    questions[2].correct_option = 'a';
 
     return questions;
 }
 
 // Questions pour le thème C
-QuizQuestion* createCQuiz(int *size) {
+QuizQuestion *createCQuiz(int *size)
+{
     *size = 3;
     QuizQuestion *questions = malloc(*size * sizeof(QuizQuestion));
 
     questions[0].question = strdup("Quel est le résultat de l'expression suivante en C : 5 * 3 + 2 / 2 - 1?");
-    questions[0].options[0] = strdup("A. 15");
-    questions[0].options[1] = strdup("B. 7");
-    questions[0].options[2] = strdup("C. 8");
-    questions[0].options[3] = strdup("D. 9");
-    questions[0].correct_option = 'A';
+    questions[0].options[0] = strdup("a. 15");
+    questions[0].options[1] = strdup("b. 7");
+    questions[0].options[2] = strdup("c. 8");
+    questions[0].options[3] = strdup("d. 9");
+    questions[0].correct_option = 'a';
 
     questions[1].question = strdup("Quel est le mot-clé pour allouer de la mémoire dynamiquement en C?");
-    questions[1].options[0] = strdup("A. malloc");
-    questions[1].options[1] = strdup("B. allocate");
-    questions[1].options[2] = strdup("C. memalloc");
-    questions[1].options[3] = strdup("D. new");
-    questions[1].correct_option = 'A';
+    questions[1].options[0] = strdup("a. malloc");
+    questions[1].options[1] = strdup("b. allocate");
+    questions[1].options[2] = strdup("c. memalloc");
+    questions[1].options[3] = strdup("d. new");
+    questions[1].correct_option = 'a';
 
     questions[2].question = strdup("Quel est l'opérateur utilisé pour accéder aux membres d'une structure via un pointeur?");
-    questions[2].options[0] = strdup("A. . (point)");
-    questions[2].options[1] = strdup("B. -> (flèche)");
-    questions[2].options[2] = strdup("C. * (étoile)");
-    questions[2].options[3] = strdup("D. & (esperluette)");
-    questions[2].correct_option = 'B';
+    questions[2].options[0] = strdup("a. . (point)");
+    questions[2].options[1] = strdup("b. -> (flèche)");
+    questions[2].options[2] = strdup("c. * (étoile)");
+    questions[2].options[3] = strdup("d. & (esperluette)");
+    questions[2].correct_option = 'b';
 
     return questions;
 }
 
 // Questions pour le thème Javascript
-QuizQuestion* createJavaScriptQuiz(int *size) {
+QuizQuestion *createJavaScriptQuiz(int *size)
+{
     *size = 3;
     QuizQuestion *questions = malloc(*size * sizeof(QuizQuestion));
 
     questions[0].question = strdup("Quel mot-clé est utilisé pour déclarer une variable en Javascript?");
-    questions[0].options[0] = strdup("A. var");
-    questions[0].options[1] = strdup("B. int");
-    questions[0].options[2] = strdup("C. let");
-    questions[0].options[3] = strdup("D. var et let");
-    questions[0].correct_option = 'D';
+    questions[0].options[0] = strdup("a. var");
+    questions[0].options[1] = strdup("b. int");
+    questions[0].options[2] = strdup("c. let");
+    questions[0].options[3] = strdup("d. var et let");
+    questions[0].correct_option = 'd';
 
-    questions[1].question = strdup("Comment crée-t-on une fonction anonyme en Javascript?");
-    questions[1].options[0] = strdup("A. function() {}");
-    questions[1].options[1] = strdup("B. anonymous function() {}");
-    questions[1].options[2] = strdup("C. () => {}");
-    questions[1].options[3] = strdup("D. function => {}");
-    questions[1].correct_option = 'A';
+    questions[1].question = strdup("comment crée-t-on une fonction anonyme en Javascript?");
+    questions[1].options[0] = strdup("a. function() {}");
+    questions[1].options[1] = strdup("b. anonymous function() {}");
+    questions[1].options[2] = strdup("c. () => {}");
+    questions[1].options[3] = strdup("d. function => {}");
+    questions[1].correct_option = 'a';
 
     questions[2].question = strdup("Quel est l'opérateur de concaténation de chaînes en Javascript?");
-    questions[2].options[0] = strdup("A. +");
-    questions[2].options[1] = strdup("B. &");
-    questions[2].options[2] = strdup("C. .");
-    questions[2].options[3] = strdup("D. =");
-    questions[2].correct_option = 'A';
+    questions[2].options[0] = strdup("a. +");
+    questions[2].options[1] = strdup("b. &");
+    questions[2].options[2] = strdup("c. .");
+    questions[2].options[3] = strdup("d. =");
+    questions[2].correct_option = 'a';
 
     return questions;
 }
 
 // Libère la mémoire des questions
-void freeQuiz(QuizQuestion *questions, int size) {
-    for (int i = 0; i < size; i++) {
->>>>>>> Stashed changes
+void freeQuiz(QuizQuestion *questions, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         free(questions[i].question);
         for (int j = 0; j < 4; j++)
         {
@@ -180,11 +174,24 @@ char askQuestion(QuizQuestion *q, int questionNumber)
     {
         printf("%s\n", q->options[i]);
     }
-    printf("Votre réponse (A, B, C, D) : ");
 
     char answer;
-    scanf(" %c", &answer);
-    return answer;
+    while (1)
+    {
+        printf("Votre réponse (a, b, c, d) : ");
+        if (scanf(" %c", &answer) != 1 || (answer != 'a' && answer != 'b' && answer != 'c' && answer != 'd'))
+        {
+            printf("Réponse invalide. Veuillez entrer a, b, c ou d.\n");
+            while (getchar() != '\n')
+                ; // vider le tampon d'entrée
+        }
+        else
+        {
+            while (getchar() != '\n')
+                ; // vider le tampon d'entrée
+            return answer;
+        }
+    }
 }
 
 void startQuiz(QuizQuestion *questions, int size)
@@ -195,7 +202,7 @@ void startQuiz(QuizQuestion *questions, int size)
         char userAnswer = askQuestion(&questions[i], i);
         if (userAnswer == questions[i].correct_option)
         {
-            printf("Correct!\n");
+            printf("correct!\n");
             score++;
         }
         else
